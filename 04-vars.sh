6b11e5c19aaa292f220a40845c45be60dbf76e5e
Sushma <sushma.gayatri451@gmail.com>
2022-09-28 09:34:44 +0530

    Print variables

--- 04-vars.sh
+++ 04-vars.sh
@@ -15,3 +15,14 @@ echo "${b}"
 # all of them mean the same to print the value
 
 echo value of d is : $d
+
+# when the data is not mentioned its a null value
+
+# what ever we are running on xshell it runs on heat memoty when some value is declared  on export
+# its the value till the machine is up once we exit and relogin the valu has to be declared again
+# export will only be considered when its not declafred locally
+# local values have high priority
+# variables declared in the program have high priority than those declared on export
+
+DATE=2022-09-05
+echo "Good Morning, Todays date is $DATE"
