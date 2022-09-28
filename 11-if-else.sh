ab67b8c6a97205db0f286d53567bc1585145d76a
Sushma <sushma.gayatri451@gmail.com>
2022-09-28 21:26:36 +0530

    Print Conditions

--- 11-if-else.sh
+++ 11-if-else.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 ACTION=$1 # expects value from command line
+# if we use doubal quotes we need to use on both the sides of equal to
+# if we use single quotes it kills the value of the variable
 
-if [ "$ACTION" = "start" ] ; then # Delaing with strings hence using '=' if we use numbers we use 'eq for equal to'
-    echo "Selected choice is start"
+# Demo on if Else
+# if [ "$ACTION" = "start" ]; then
+#   echo "Selected choice is start"
 
-else 
-    echo "Only valid option is start"
+# else
+#   echo "Only valid option is start"
+#fi
+
+# Demo on else if
+
+if [ "$ACTION" = "start" ]; then
+   echo "Starting ABC Service"
+
+elif [ "$ACTION" = "stop" ]; then
+    echo "Stopping ABC Service"
+
+elif [ "$ACTION" = "restart" ]; then
+    echo "Restarting ABC Service"
+
+else
+   echo "Only valid options are \e[34mstart or stop or restart only \e[0n"
 fi
