ba66b6c8562258b3917a134bd827cc030c2d278d
Sushma <sushma.gayatri451@gmail.com>
2022-09-28 20:45:58 +0530

    Print Conditions

--- 10-comments.sh
+++ 10-comments.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 echo "cloud Training"
+# called as multi line comment <<COMMENT ends with COMMENT
 <<COMMENT
 echo "DevOps Training"
 echo "Cloud DevOps Training"
 a=10
 echo $a
 COMMENT
+
+echo "cloud Training"
+echo "Cloud DevOps Training"
+echo "Cloud DevOps Training"
+a=10
+echo $a
