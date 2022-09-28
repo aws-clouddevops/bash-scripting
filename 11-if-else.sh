e481c406ef73e9b6486976a053a139963688acc9
Sushma <sushma.gayatri451@gmail.com>
2022-09-28 21:11:50 +0530

    Print Conditions

--- 11-if-else.sh
+++ 11-if-else.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ACTION=$1
+ACTION=$1 # expects value from command line
 
-if [ "$ACTION" = "start" ] ; then
+if [ "$ACTION" = "start" ] ; then # Delaing with strings hence using '=' if we use numbers we use 'eq for equal to'
     echo "Selected choice is start"
 
+else 
+    echo "Only valid option is start"
 fi
