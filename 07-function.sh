5a558a6349a7f68853cade66bec2d0d67276b9b5
Sushma <sushma.gayatri451@gmail.com>
2022-09-28 14:45:07 +0530

    Print Functions

--- 07-function.sh
+++ 07-function.sh
@@ -8,6 +8,18 @@ sample() {
 
 sample
 
-sample
+# Defining the function stat
 
-sample
+stat(){
+    echo "load average on the system from last 1 min is : uptime | awk -F : '{print $NF}' | awk -F , '{print $1}'
+"
+    echo "Number of users signed in is : $(who|wc -1)"
+    echo "Stat Function is completed bye"
+
+}
+
+# calling the function
+stat
+sleep 5 # the function pauses for 5 sec
+stat
+sleep 5
