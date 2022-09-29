eb44c27e3a507fc19547dc1aee79f8d82dd4b9d0
Sushma <sushma.gayatri451@gmail.com>
2022-09-29 08:38:54 +0530

    Print check

--- 12-check.sh
+++ 12-check.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "Excecuting httpd installation"
-yum install httpd -y
+ID=$(id -u)
+
+if[ $ID -eq 0]; then
+    echo "Excecuting httpd installation"
+    yum install httpd -y
+
+else
+    echo -e "\e[31m Try executing the script with a sudo access \e[0m"
+
+fi
