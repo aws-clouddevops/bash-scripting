241100b63b7355cbf85b344b5309b37174e3b966
Sushma <sushma.gayatri451@gmail.com>
2022-10-07 23:40:24 +0530

    mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-ID=$(id -u)
+source components/common.sh
 
-if [ $ID -ne 0 ]; then
-    echo -e "\e[31m Try executing the script with a sudo access \e[0m"
-    exit 1
-fi
+COMPONENT=mongodb
+
+echo -n "Configuring the MongoDb repo: "
+curl -s -o /etc/yum.repos.d/mon${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
+stat $? # calling function with variable
+
+echo -n "Installing ${COMPONENT}: "
+yum install -y mongodb-org
+stat $?
+
+echo -n "updating the $COMPONENT config: "
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat $?
+
+echo -n "Starting ${COMPONENT} service: "
+systemctl enable mongod >>/tmp/${COMPONENT}.log
+systemctl start mongod
+stat $?
