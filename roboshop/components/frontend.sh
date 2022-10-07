3197651c8b13f4bd2bd6cc6fe0485ac9bdeb29d6
Sushma <sushma.gayatri451@gmail.com>
2022-10-07 23:04:13 +0530

    frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -11,22 +11,30 @@ echo -n "Installing Nginx: "
 yum install nginx -y  >> /tmp/frontend.log
 systemctl enable nginx
 
-echo -n "Starting Nginx:"
+echo -n "Starting Nginx: "
 systemctl start nginx
 stat $?
 
-echo -n "Downloading the Code"
+echo -n "Downloading the Code: "
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $?
 
 cd /usr/share/nginx/html
 rm -rf *
+echo -n "Extracting the Zip File: "
 unzip  -o /tmp/frontend.zip  >> /tmp/frontend.log
+stat $?
+
 mv frontend-main/* .
 mv static/* .
+echo -n "Performing Cleanup: "
 rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
-echo -n "Restarting Nginx"
+echo -n "Configuring the reverse proxy: "
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "Restarting Nginx: "
 systemctl restart nginx
 stat $?
