2292bfc2d213af0c6054f55f497dbc660872238c
Sushma <sushma.gayatri451@gmail.com>
2022-10-07 14:04:45 +0530

    frontend

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,14 +5,14 @@ set -e # ensures your script will stop if any of the instruction fails
 # source is an import command , it imports the code and runs locally
 source components/common.sh
 
-yum install nginx -y >> /tmp/frontend.log
+yum install nginx -y  >> /tmp/frontend.log
 systemctl enable ningx
 systemctl start ningx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip  /tmp/frontend.log
+unzip /tmp/frontend.zip   /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
