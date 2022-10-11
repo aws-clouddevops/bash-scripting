38bf9eb35d93648f9d74d51eb12d263592f9f8b8
Sushma <sushma.gayatri451@gmail.com>
2022-10-11 11:23:27 +0530

    User

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -16,9 +16,11 @@ stat $?
 
 echo -n "Whitelisting the ${COMPONENT} config: "
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf >> /tmp/${COMPONENT}.log
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf >> /tmp/${COMPONENT}.log
 stat $?
 
 echo -n "Starting $COMPONENT: "
-systemctl enable redis >> /tmp/${COMPONENT}.log >> /tmp/${COMPONENT}.log
-systemctl start redis >> /tmp/${COMPONENT}.log >> /tmp/${COMPONENT}.log
+systemctl daemon redis &>> /tmp/${COMPONENT}.log >> /tmp/${COMPONENT}.log
+systemctl enable redis &>> /tmp/${COMPONENT}.log >> /tmp/${COMPONENT}.log
+systemctl start redis  &>> /tmp/${COMPONENT}.log >> /tmp/${COMPONENT}.log
 stat $?
