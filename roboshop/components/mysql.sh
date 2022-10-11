25f357cab38e40f835c8094a1e6c0a3255a35329
Sushma <sushma.gayatri451@gmail.com>
2022-10-11 17:40:12 +0530

    mysql

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -7,14 +7,14 @@ source components/common.sh
 COMPONENT=mysql
 
 echo -n "Configuring the ${COMPONENT} repo:"
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo $>> ${LOGFILE}
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> ${LOGFILE}
 stat $?
 
 echo -n "Installing ${COMPONENT}: "
-yum install mysql-community-server -y $>>${LOGFILE}
+yum install mysql-community-server -y &>>${LOGFILE}
 stat $?
 
 echo -n "Starting ${COMPONENT}: "
-systemctl enable mysqld $>>${LOGFILE}
-systemctl start mysqld $>>${LOGFILE}
+systemctl enable mysqld &>>${LOGFILE}
+systemctl start mysqld &>>${LOGFILE}
 stat $?
