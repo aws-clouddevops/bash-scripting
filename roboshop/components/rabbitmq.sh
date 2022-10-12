882e3e10cedf92b5e216e4a3a7cbca7677a7adaf
Sushma <sushma.gayatri451@gmail.com>
2022-10-12 12:15:02 +0530

    rabbitmq

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -4,11 +4,8 @@ source components/common.sh
 
 COMPONENTS=rabbitmq
 
-echo -n "Installing Erland Dependencies: "
+echo -n "Installing and configuring Erland Dependencies: "
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> ${LOGFILE}
-stat $?
-
-echo -n "Configuring the yum repo for ${COMPONENT}: "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> ${LOGFILE}
 stat $?
 
@@ -21,8 +18,8 @@ systemctl enable rabbitmq-server &>> ${LOGFILE}
 systemctl start rabbitmq-server &>> ${LOGFILE}
 stat $?
 
-rabbitmqct list_users | grep roboshop &>> ${LOGFILE}
-if [ $? -eq 0 ]; then
+rabbitmqctl list_users | grep roboshop &>> ${LOGFILE}
+if [ $? -ne 0 ]; then
     echo -n "Creating ${COMPONENT} Application User: "
     rabbitmqctl add_user roboshop roboshop123 &>> ${LOGFILE}
 stat $?
