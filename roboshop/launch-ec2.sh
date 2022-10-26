eba2686be54aeb28c2ee1cac25bac5de9e57e22d
Sushma <sushma.gayatri451@gmail.com>
2022-10-26 13:04:36 +0530

    Launch ec2

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -5,7 +5,7 @@
 # If $ is empty or $1 is not supplied, then I want to mark it as follows
 
 
-if [ -Z "$1"]; then
+if [ -z "$1" ]; then
     echo -e "\e[31m Machine Name is Missing \e[0m"
     exit 1
 fi
@@ -17,4 +17,4 @@ SGID="sg-0b01a705773cff66c"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "This is the AMI id we are using $AMI_ID"
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" |jq
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SGID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options {"SpotInstanceType": "persistant","InstanceInterruptionBehaviour":"stop"}|jq
