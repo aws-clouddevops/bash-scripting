d979af862c12b7b52071e95ba94e36df815f8c15
Sushma <sushma.gayatri451@gmail.com>
2022-10-26 12:55:05 +0530

    Launch ec2

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-COMPONENT=$1
 
 # AMI_ID=ami-00ff427d936335825
+# If $ is empty or $1 is not supplied, then I want to mark it as follows
+
+
+if [ -Z "$1"]; then
+    echo -e "\e[31m Machine Name is Missing \e[0m"
+    exit 1
+fi
+
+
+COMPONENT=$1
 SGID="sg-0b01a705773cff66c"
 
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
