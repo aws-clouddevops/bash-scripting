38b105c2c8f3f964ef1dff4ab90efa86a631f3db
Sushma <sushma.gayatri451@gmail.com>
2022-11-22 20:21:26 +0530

    launch

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -15,7 +15,7 @@ COMPONENT=$1
 ENV=$2
 SGID="sg-0b01a705773cff66c"
 ZONEID="Z01423552ZMVW82NC77OX"
-AMI_ID=ami-00ff427d936335825
+AMI_ID="ami-00ff427d936335825"
 # AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "This is the AMI id we are using $AMI_ID"
 
